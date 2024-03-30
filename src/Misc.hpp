bad9707a8a0b289ac29a874ed60a9e310f352b3d
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2024-03-30 23:03:48 +0700

    MISC

--- src/Misc.hpp
+++ src/Misc.hpp
@@ -1,20 +1,25 @@
+#ifndef MISC_HPP
+#define MISC_HPP
+
 #include <iostream>
-
 class Misc {
-public:
-    Misc(int winning_money, int winning_weight, std::pair<int, int> storage_size, std::pair<int, int> field_size, std::pair<int, int> farm_size)
-        : winning_money(winning_money), winning_weight(winning_weight), storage_size(storage_size), field_size(field_size), farm_size(farm_size) {}
-
-    int getWinningMoney() const { return winning_money; }
-    int getWinningWeight() const { return winning_weight; }
-    std::pair<int, int> getStorageSize() const { return storage_size; }
-    std::pair<int, int> getFieldSize() const { return field_size; }
-    std::pair<int, int> getFarmSize() const { return farm_size; }
-
 private:
     int winning_money;
     int winning_weight;
     std::pair<int, int> storage_size;
     std::pair<int, int> field_size;
     std::pair<int, int> farm_size;
+    
+public:
+    Misc(int winning_money, int winning_weight, std::pair<int, int> storage_size, std::pair<int, int> field_size, std::pair<int, int> farm_size);
+
+    int getWinningMoney() const;
+    int getWinningWeight() const;
+    std::pair<int, int> getStorageSize() const;
+    std::pair<int, int> getFieldSize() const;
+    std::pair<int, int> getFarmSize() const;
+
+    friend std::ostream& operator<<(std::ostream& os ,Misc& m);
 };
+
+#endif
